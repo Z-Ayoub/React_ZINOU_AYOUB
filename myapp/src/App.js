14c942686d0e66bcffc1637a480d490e8e5e6854
Z-Ayoub <ayoub.zinou@ecole-isitech.fr>
2022-03-28 15:23:47 +0200

    Main

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import Test from './components/Test'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// Update VUE
+const App = () => {
+const [myVar, setMyVar] = useState('Hello World');
+const [movies, setMovies] = useState('');
+
+function fetchMovieHandler() {
+  fetch('https://swapi.dev/api/films/').then(response=>{
+  console.log(response);
+  response.json();
+ })
+ }
+ useEffect(() => {
+   fetchMovieHandler();
+ },[]);
+
+const onClickHandler = () => {
+  setMyVar('Hello World updated');
+ 
+  //2ème méthode
+  setMyVar((prevState) => {
+     return prevState + ' updated';
+  })
+ }
+
+const  childToParentUpdater = (data) => {
+  console.log('childToParentUpdater',data);
+}
+
+
+return (
+  <div className="App">
+    <button onClick={onClickHandler}>ClickMe</button>
+    {myVar}
+    {/* <Test myProps='hello' /> */}
+    <Test myProps={myVar} updater={childToParentUpdater}/>
+    {[1,2,5,9]}
+  </div>
+  
+);
 }
 
 export default App;
